6d18b447536ad20f0c1d1699171d033905cae7c9
Vu2104 <phamthachanhvu@gmail.com>
2025-12-23 15:41:42 -0800

    Updated main

--- CatWalk/urls.py
+++ CatWalk/urls.py
@@ -19,9 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
     path('',include('stockpicker.urls')) 
-=======
-    path('',include('stockpicker.urls')),
->>>>>>> catwalk-algorithm
 ]
--- stockpicker/urls.py
+++ stockpicker/urls.py
@@ -4,13 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.index, name = 'view'),       # index page with 10x10 grid
 
-<<<<<<< HEAD
     path('pick/',views.pick, name = 'pick'),   #  this is the picked stock page
-=======
-    path('stockpicker/',views.stockPickerPage, name = 'stockpicker'),   #  URL for stockpicker
-
-    path('pickstock/', views.pickStock, name = 'pickstock'), # URL for pickStock view
->>>>>>> origin/final-project-version
 
     path('reset/',views.reset, name = 'reset'), # URL for resetting the page
 
