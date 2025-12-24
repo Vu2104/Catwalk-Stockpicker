08c293eb7f1808ed0367d9f865be0d0d7089dbbb
Vu2104 <phamthachanhvu@gmail.com>
2025-12-23 16:38:07 -0800

    Updated main

--- stockpicker/urls.py
+++ stockpicker/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.index, name = 'view'),       # index page with 10x10 grid
 
-    path('pick/',views.pick, name = 'pick'),   #  this is the picked stock page
+    path('stockpicker/',views.stockPickerPage, name = 'stockpicker'),   #  URL for stockpicker
+
+    path('pickstock/', views.pickStock, name = 'pickstock'), # URL for pickStock view
 
     path('reset/',views.reset, name = 'reset'), # URL for resetting the page
 
